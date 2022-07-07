b25b6f43e7624bfd40854bc5f170d58495489fd3
Mohana Sundari V <mohanasundari@HweServer.mirafra.com>
2022-07-07 05:55:29 +0530

    Created the FIFO sequence file in format

--- src/verif/bfm/write_fifo/fifo_seq.sv
+++ src/verif/bfm/write_fifo/fifo_seq.sv
@@ -1,47 +1,63 @@
-`uvm_object_utils(fifo_sequence)
+`ifndef FIFO_SEQ_INCLUDED_
+`define FIFO_SEQ_INCLUDED_
 
- data_transaction tr;
+//--------------------------------------------------------------------------------------------
+// Class: fifo_seq 
+// creating fifo_seq class extends from uvm_sequence
+// This class provides the FIFO basic write_read sequence
+//--------------------------------------------------------------------------------------------
+class fifo_seq extends uvm_sequence #(axi_packet);
 
- function new(string name = "fifo_sequence");
+  //factory regispktation
+  `uvm_object_utils(fifo_seq)
+  
+  //Declare the axi_packet 
+  axi_packet pkt;
+  
+  //-------------------------------------------------------
+  // Externally defined Function
+  //-------------------------------------------------------
+  extern function new(spkting name = "fifo_seq");
+  extern task body();
 
+endclass : fifo_seq
+
+//-----------------------------------------------------------------------------
+// Conspktuctor: new
+// Initializes fifo_sequence class object
+//
+// Parameters:
+//  name - fifo_seq
+//-----------------------------------------------------------------------------
+function fifo_seq::new(spkting name = "fifo_seq");
   super.new(name);
+endfunction : new
 
- endfunction
-
- task body();
-
-  tr = data_transaction::type_id::create("tx_data_tr");
-
-  start_item(tr);
-
-   for (int i = 0; i < tr.Fifo_depth; i++)
-
-   begin
-
-   if(tr.wvalid==1 && tr.wready==1)
-
-   begin
-
-   if (!tr.randomize() with {(tr.we == 1'b1 && tr.re == 1'b0) -> tr.wdata == WDATA;})
-
-       `uvm_error("Sequence", "Randomization failure for trasaction")
-
-   end
-
-  else if (rvalid==1 && rready==1)
-
-  begin
-
-  if (!tr.randomize() with {(tr.we == 1'b0 && tr.re == 1'b1) -> tr.radata == RDATA;})
-
-     `uvm_error("Sequence", "Randomization failure for trasaction")
-
+//--------------------------------------------------------------------------------------------
+// Task: body
+// Creates the pkt of type axi_packet and randomises the pkt
+//--------------------------------------------------------------------------------------------
+task fifo_seq::body();
+  if(wvalid==1 && wready==1)begin
+  for (int i = 0; i < pkt.fifo_depth; i++)begin 
+  pkt = axi_packet::type_id::create("pkt");
+  
+  start_item(pkt);
+  if (i < 20)begin
+  // if (!pkt.randomize() with {pkt.put == 1'b1; pkt.get == 1'b0;}) begin
+  if (!pkt.randomize() with {(pkt.we == 1'b1 && pkt.re == 1'b0) -> pkt.wdata == WDATA;}) begin
+  `uvm_error("Sequence", "Randomization failure for transaction")
+  end
+  end
+  else if (rvalid==1 && rready==1)begin
+  //if (!pkt.randomize() with {pkt.put == 1'b0; pkt.get == 1'b1;}) begin
+  if (!pkt.randomize() with {(pkt.we == 1'b0 && pkt.re == 1'b1) -> pkt.rdata == RDATA;}) begin
+  `uvm_error("Sequence", "Randomization failure for transaction")
+  end
+  end
+  finish_item(pkt);
   end
 
-  end
+endtask : body
 
-  finish_item(tr);
-
-  endtask
-
-endclass : fifo_sequence
+`endif
