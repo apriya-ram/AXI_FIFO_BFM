b92cdfd5e6f5284bbe028ebb8023eefc6562c2b8
apriya-ram <61039744+apriya-ram@users.noreply.github.com>
2023-12-18 14:40:55 +0530

    Revert "Shahid UPDATED VERSION"

--- sim/synopsys_sim/csrc/uvm_verdi_dpi.d
+++
@@ -1,8 +0,0 @@
-uvm_verdi_dpi.o: \
- /hwetools/synosys/installed_tools/vcs/Q-2020.03-SP2-8/etc/uvm-1.2/verdi/dpi/uvm_verdi_dpi.cpp \
- /hwetools/synosys/installed_tools/vcs/Q-2020.03-SP2-8/include/vpi_user.h \
- /hwetools/synosys/installed_tools/vcs/Q-2020.03-SP2-8/include/veriuser.h \
- /hwetools/synosys/installed_tools/vcs/Q-2020.03-SP2-8/include/sv_vpi_user.h \
- /hwetools/synosys/installed_tools/vcs/Q-2020.03-SP2-8/include/vpi_user.h \
- /hwetools/synosys/installed_tools/vcs/Q-2020.03-SP2-8/include/vcs_vpi_user.h \
- /hwetools/synosys/installed_tools/vcs/Q-2020.03-SP2-8/include/sv_vpi_user.h
